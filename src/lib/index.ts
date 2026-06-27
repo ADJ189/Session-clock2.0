70a547ac7f74b2db99e1ebdfac891dafaa83b806
ADJ <funnyhut3@gmail.com>
2026-06-27 20:26:53 +0530

    Create index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+// Re-export key stores for convenience
+export { settings } from './state/settings';
+export { weatherData, hourlyForecast, dailyForecast, sunTimes } from './state/weather';
+export { currentTheme } from './state/theme';
+export { tabs, activeTab } from './state/nav';
